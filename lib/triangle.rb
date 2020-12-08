008bf5cc2df9e4e05db5fe3d53d573a2f50f1f82
+ <+>
2020-12-08 15:20:15 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+  
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three    
+  end
+
+  def invalid?
+    if (side_one + side_two) <= side_three || (side_one + side_three) <= side_two || (side_two + side_three) <= side_one || side_one < 0 || side_two < 0 || side_three < 0
+      true
+    else
+      false
+    end
+  end
+
+
+  def kind
+    if invalid?
+       raise TriangleError
+    elsif side_one == side_two && side_two == side_three && side_one == side_three
+        :equilateral
+    elsif side_one == side_two || side_one == side_three || side_two == side_three
+        :isosceles
+    else
+        :scalene
+    end
+  end
+  #binding.pry
+
+  class TriangleError < StandardError
+    "this triangle is invalid"
+  end
+
 end
